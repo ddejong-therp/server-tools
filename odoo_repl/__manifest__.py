d16a18926ae74f2823bf0b1294ee0e1a4f560985
Danny de Jong <ddejong@therp.nl>
2025-09-16 17:37:45 +0200

    [ADD] Add the odoo_repl module

---
+++ odoo_repl/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2025 Therp BV (https://www.therp.nl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Odoo REPL",
+    "summary": "Extends the Odoo interactive shell with functionality to display "
+               "diagnostic information on models and where everything is defined.",
+    "version": "18.0.1.0.0",
+    "author": "Therp BV, "
+        "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "maintainer": "Therp BV",
+    "category": "Extra Tools",
+    "website": "https://github.com/OCA/server-tools",
+    "external_dependencies": {
+        "python": ["odoo_repl"],
+    },
+    "auto_install": False,
+    "installable": True,
+}
